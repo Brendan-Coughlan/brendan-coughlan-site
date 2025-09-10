ca26c927cf407ea3d037f848b2a131f7b51e9320
Brendan Coughlan <bcoughlan2404@gmail.com>
2025-09-10 17:23:00 -0400

    Changed from static site to dynamic

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+if __name__ == '__main__':
+    app.run()
